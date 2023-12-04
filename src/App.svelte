92f24cf7f76973bd6cc827916e1f818e24da3cbe
Replit user <>
2023-12-04 21:49:36 +0000

    add movie component

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script>
+  import Movie from './Movie.svelte';
 	export let name;
 
   const sendRequest = async (url) => {
@@ -30,16 +31,8 @@
   {#await fetchMovie()}
     <p>Loading Movies...</p>
   {:then movieResponse}
-    {#each movieResponse.movies as {title,poster,douban_rating}}
-    <article class="movie">
-      <img class="movie_poster" src={poster} alt={title} />
-      <section class="movie_info">
-        <h4>{title}</h4>
-        {#if douban_rating}
-        <span class="movie_rating">{douban_rating}</span>
-        {/if}
-      </section>
-    </article>
+    {#each movieResponse.movies as movie}
+    <Movie movie={movie} />
     {/each}
   {:catch error}
     <p>Movie Error</p>
@@ -61,27 +54,6 @@
 		font-size: 4em;
 		font-weight: 100;
 	}
-  .movie {
-    display: flex;
-    margin-top: 2rem;
-    position: relative;
-  }
-  .movie_poster {
-    width: 30%;
-    border-radius: 10px;
-  }
-  .movie_info {
-    padding: 10px;
-  }
-  .movie_rating {
-    border-radius: 5px;
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    padding: 5px;
-    color: #fff;
-    background-color: seagreen;
-  }
 
 	@media (min-width: 640px) {
 		main {
