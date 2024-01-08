8d172c098742f2fd08a887882bb4dbf039fc9c36
09liweis <weisen.li@hotmail.com>
2024-01-08 10:18:33 -0500

    update boxOffice text name

--- src/contants.js
+++ src/contants.js
@@ -1,12 +1,12 @@
-const boxOffice = "Box Office";
+const boxOfficeTxt = "Box Office";
 export const Categories = [
   { tl: "Popular", nm: "popular" },
   { tl: "In Theatre", nm: "in_theatre" },
   { tl: "In Comming", nm: "comming" },
   { tl: "Chart", nm: "chart" },
-  { tl: `MaoYan ${boxOffice}`, nm: "maoyan" },
-  { tl: `All Time ${boxOffice}`, nm: "alltime_boxoffice" },
-  { tl: `IMDB ${boxOffice}`, nm: "imdb_boxoffice" },
+  { tl: `MaoYan ${boxOfficeTxt}`, nm: "maoyan" },
+  { tl: `All Time ${boxOfficeTxt}`, nm: "alltime_boxoffice" },
+  { tl: `IMDB ${boxOfficeTxt}`, nm: "imdb_boxoffice" },
 ];
 
 export const MOVIES_API = "https://samliweisen.onrender.com/api/movies/";
