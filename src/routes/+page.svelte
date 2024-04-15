4c6297dfc489023fa0b24f8e30004d8ded81b559
09liweis <weisen.li@hotmail.com>
2024-04-15 12:09:34 -0400

    add svelte click button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  let title = "Rick and Morty Library";
+  const updatTitle = () => {
+    title = "Rick and Morty Done";
+  }
+</script>
+<h1>Welcome to {title}</h1>
+<button on:click={updatTitle}>Click</button>
