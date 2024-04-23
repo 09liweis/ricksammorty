4aa08b8a21f412550404c0e0ed848a25e0f4d1cb
09liweis <weisen.li@hotmail.com>
2024-04-23 14:43:39 -0400

    add graphql

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,46 @@
   const updatTitle = () => {
     title = "Rick and Morty Done";
   }
+
+  let characters = [];
+  
+  import { request, gql } from 'graphql-request'
+
+  const document = gql`
+    {
+      characters(page: 1) {
+        info {
+          count
+        }
+        results {
+          id
+          name
+          status
+          species
+          gender
+          image
+          episode {
+            name
+            episode
+          }
+          location {
+            name
+          }
+        }
+      }
+    }
+  `
+  request('https://rickandmortyapi.com/graphql', document).then((resp)=>{
+    characters = resp.characters.results;
+  }).catch((err)=>{
+
+  });
 </script>
 <h1>Welcome to {title}</h1>
 <button on:click={updatTitle}>Click</button>
+{#each characters as c}
+<article>
+  <img src={c.image} />
+  <h2>{c.name}</h2>
+</article>
+{/each}
