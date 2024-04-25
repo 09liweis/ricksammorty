a78083e48994af2af9a7e2225603427d7e06077e
09liweis <weisen.li@hotmail.com>
2024-04-25 15:10:32 -0400

    add character type

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,9 @@
 // place files you want to import through the `$lib` alias in this folder.
+export type Character = {
+  id:string,
+  name:string,
+  status:string,
+  species:string,
+  gender:string
+  image:string
+}
