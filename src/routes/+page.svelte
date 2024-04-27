49246bade87a8c93d21e3ce2e08a0754289b4852
09liweis <weisen.li@hotmail.com>
2024-04-26 22:39:44 -0400

    style character card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,8 +20,8 @@
 <button on:click={updatTitle}>Click</button>
 <section class="flex flex-wrap">
 {#each characters as c}
-<article class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6">
-  <img src={c.image} alt={c.name}/>
+<article class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 p-1">
+  <img src={c.image} alt={c.name} class="rounded"/>
   <h2>{c.name}</h2>
   <p>{c.status}</p>
   <p>{c.species}</p>
