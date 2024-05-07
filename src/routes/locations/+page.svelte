b466cfd1b190af5fda52f23c6f054fcc71659d51
09liweis <weisen.li@hotmail.com>
2024-05-07 13:56:45 -0400

    add locations page

---
+++ src/routes/locations/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { onMount } from 'svelte';
+  import {fetchGql} from '$lib/fetch.js';
+  
+  onMount(async() => {
+
+	});
+  
+  
+</script>
+<h1>Locations</h1>
+<section class="flex flex-wrap">
+
+</section>
