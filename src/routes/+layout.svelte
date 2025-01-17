f3ac2f799f6bac91edfbcadef97c982959f3bc84
09liweis <weisen.li@hotmail.com>
2025-01-17 13:57:39 -0500

    refactor header menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,29 +1,30 @@
 <script>
   import "../app.css";
   import { page } from '$app/stores';
+  const MENUS = [
+    {
+      tl: 'Characters', pathname: '/'
+    },
+    {
+      tl: 'Locations', pathname: '/locations'
+    },
+    {
+      tl: 'Episodes', pathname: '/episodes'
+    },
+  ];
 </script>
 
 <nav class="bg-purple-600 text-white shadow-lg max-w-7xl mx-auto px-4 flex items-center justify-between h-16">
   <a href="/" class="text-xl font-bold">Rick and Morty</a>
   <div class="flex space-x-4">
+    {#each MENUS as menu}
     <a 
-      href="/" 
-      class="px-3 py-2 rounded-md text-sm font-medium {$page.url.pathname === '/' ? 'bg-purple-800' : 'hover:bg-purple-700'}"
+      href="{menu.pathname}" 
+      class="px-3 py-2 rounded-md text-sm font-medium {$page.url.pathname === menu.pathname ? 'bg-purple-800' : 'hover:bg-purple-700'}"
     >
-      Characters
+      {menu.tl}
     </a>
-    <a 
-      href="/locations" 
-      class="px-3 py-2 rounded-md text-sm font-medium {$page.url.pathname === '/locations' ? 'bg-purple-800' : 'hover:bg-purple-700'}"
-    >
-      Locations
-    </a>
-    <a 
-      href="/episodes" 
-      class="px-3 py-2 rounded-md text-sm font-medium {$page.url.pathname === '/episodes' ? 'bg-purple-800' : 'hover:bg-purple-700'}"
-    >
-      Episodes
-    </a>
+    {/each}
   </div>
 </nav>
 
