8399b2201548f84f6480e45b5b8562a82d26c6a9
09liweis <weisen.li@hotmail.com>
2025-01-23 22:48:03 -0500

    reduce nav menu gap

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,7 +16,7 @@
 
 <nav class="sticky top-0 z-10 bg-purple-600 text-white shadow-lg max-w-7xl mx-auto px-4 flex items-center justify-between h-16">
   <a href="/" class="text-xl font-bold">Rick and Morty</a>
-  <div class="flex space-x-4">
+  <div class="flex space-x-2">
     {#each MENUS as menu}
     <a 
       href="{menu.pathname}" 
