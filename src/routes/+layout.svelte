5484f71ef24bf29904909f1cd46c86d9a28a924c
09liweis <weisen.li@hotmail.com>
2025-01-25 23:48:40 -0500

    refactor header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,18 +12,15 @@
       tl: 'Episodes', pathname: '/episodes'
     },
   ];
-  const isActive = (/** @type {string} */ currentPage, /** @type {string} */ activePathname) => {
-    return currentPage === activePathname;
-  }
 </script>
 
-<nav class="sticky top-0 z-10 bg-purple-600 text-white shadow-lg max-w-7xl mx-auto p-2 flex items-center justify-between">
-  <a href="/" class="text-sm sm:text-xl font-bold">Rick and Morty</a>
+<nav class="sticky top-0 z-10 bg-purple-600 text-white shadow-lg max-w-7xl mx-auto px-4 flex items-center justify-between h-16">
+  <a href="/" class="text-xl font-bold">Rick and Morty</a>
   <div class="flex space-x-2">
     {#each MENUS as menu}
     <a 
       href="{menu.pathname}" 
-      class="px-3 py-2 rounded-md text-sm font-medium {isActive($page.url.pathname, menu.pathname) ? 'bg-purple-800' : 'hover:bg-purple-700'}"
+      class="px-3 py-2 rounded-md text-sm font-medium {$page.url.pathname === menu.pathname ? 'bg-purple-800' : 'hover:bg-purple-700'}"
     >
       {menu.tl}
     </a>
